e4514b29c62aa57ebfbcaebdeb5ddfb8e0fb4166
ikaru19 <syafrizalizal17@gmail.com>
2023-02-09 11:46:14 +0700

    Initial Commit

---
+++ TheMovieDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovieDB
+//
+//  Created by Muhammad Syafrizal on 09/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
